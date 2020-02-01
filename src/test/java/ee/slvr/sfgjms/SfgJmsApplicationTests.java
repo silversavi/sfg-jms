32bfaf363a3d26da3450a61a44f8980fcc7a7648
Silver Savi <silver.savi@gmail.com>
2020-02-01 16:38:51 +0200

    initial commit

---
+++ src/test/java/ee/slvr/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.slvr.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
