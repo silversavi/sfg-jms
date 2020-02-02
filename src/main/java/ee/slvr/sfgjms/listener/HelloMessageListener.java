38dd63a43c55c1e012f6603372a6b63aa02bd6d0
Silver Savi <silver.savi@gmail.com>
2020-02-02 13:00:30 +0200

    adding HelloWorld message capability

---
+++ src/main/java/ee/slvr/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,25 @@
+package ee.slvr.sfgjms.listener;
+
+import ee.slvr.sfgjms.config.JmsConfig;
+import ee.slvr.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message){
+
+        System.out.println("I got a message!");
+
+        System.out.println(helloWorldMessage);
+
+    }
+}
---
+++ src/main/java/ee/slvr/sfgjms/sender/HelloSender.java
@@ -0,0 +1,29 @@
+package ee.slvr.sfgjms.sender;
+
+import ee.slvr.sfgjms.config.JmsConfig;
+import ee.slvr.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+        System.out.println("I'm sending a message");
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello world")
+                .build();
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE,message);
+        System.out.println("Message sent!");
+    }
+}
